62f03f7c1d0a65bc55ea3cd78daf0b968bfbe887
phoenix <basilbabyskaria@gmail.com>
2023-02-15 20:52:16 +0530

    final

--- background.js
+++ background.js
@@ -1,17 +1,10 @@
-// for(let i=0;i<=2;i++){
-// let BS=localStorage.getItem('buttonStatus');
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-      // if(BS==true){
         chrome.scripting.executeScript({
           files: ['contentscript.js'],
           target: { tabId: tab.id }
         })
-      // }
-      
-    
-
-  
-    }
+      }
   });
-// }
+
+
--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,15 @@
-    try{
-        let s=document.getElementById('sb_form_q');
-        // alert(s);
+let s=document.getElementById('sb_form_q');
+if((s.value[0]+s.value[1])=='+1'||(s.value[0]+s.value[1])=='+2')
+   {
+   try{
+    if(s.value.length<36){
         let d=document.getElementById('sb_form_go');
         s.value=s.value+"1";
         d.click();
-        i+=1;
+    }
+        
     }
     catch{
      console.log("sad");
     }
+}
