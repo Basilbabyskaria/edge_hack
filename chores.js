bc42d0693cfaf80ee8f5e4a9947e1f625c9f1ada
phoenix <basilbabyskaria@gmail.com>
2023-04-30 17:38:49 +0530

    +

---
+++ chores.js
@@ -0,0 +1,9 @@
+try{
+    
+    let x=document.querySelectorAll(".mee-icon-AddMedium");
+    for(i=0;1<x.length;i++){
+        x[i].click();
+    }
+}catch{
+
+}
--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,31 @@
-let s=document.getElementById('sb_form_q');
-if((s.value[0]+s.value[1])=='+1'||(s.value[0]+s.value[1])=='+2')
-   {
-   try{
-    if(s.value.length<36){
-        let d=document.getElementById('sb_form_go');
-        s.value=s.value+"1";
-        d.click();
+
+if(window.location.href.match("https://www.bing.com/")){
+
+    let s=document.getElementById('sb_form_q');
+    if((s.value[0]+s.value[1])=='+1'||(s.value[0]+s.value[1])=='+2')
+       {
+       try{
+        if(s.value.length<40){
+            let d=document.getElementById('sb_form_go');
+            s.value=s.value+"1";
+            d.click();
+        }
+
+        }
+        catch{
+         console.log("Done");
+        }
     }
+}
+if(window.location.href.match( "//rewards.bing.com/")){
+    try{
+    
+        let x=document.querySelectorAll(".mee-icon-AddMedium");
+        for(i=0;1<x.length;i++){
+            x[i].click();
+        }
+    }catch{
+        console.log("very sad");
         
-    }
-    catch{
-     console.log("sad");
     }
 }
