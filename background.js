3fbff683e136d95d042aef53d92785b2f72a8b6b
phoenix <basilbabyskaria@gmail.com>
2025-05-21 15:27:45 +0530

    .

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        chrome.scripting.executeScript({
-          files: ['contentscript.js'],
-          target: { tabId: tab.id }
-        })
-      }
-  });
-
-
+  if (
+    changeInfo.status == "complete" &&
+    tab.url.startsWith("https://basilbabyskaria.github.io/EdgeHackMobile")
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      func: () => {
+        const buttons = document.querySelectorAll("button");
+        let button = null;
+        buttons.forEach((button) => {
+          if (button.textContent.trim() === "Start 2") {
+            button.click();
+          }
+        });
+      },
+    });
+  }
+});
--- chores.js
+++
@@ -1,9 +0,0 @@
-try{
-    
-    let x=document.querySelectorAll(".mee-icon-AddMedium");
-    for(i=0;1<x.length;i++){
-        x[i].click();
-    }
-}catch{
-
-}
